5237c14c014baece16b716f0b29ad83afed5028c
islamlarbi7 <islamlarbi785@gmail.com>
2024-10-12 19:44:05 +0200

    none

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
-let object1 = { name: "John", age: 30, city: "New York" };
-let object2 = { name: "Jane", age: 25, city: "San Francisco" };
-let object3 = { name: "Jack", age: 40, city: "Los Angeles" };
-let object4 = { name: "Jill", age: 35, city: "Chicago" };
-let object5 = { name: "James", age: 45, city: "Miami" };
+// let object1 = { name: "John", age: 30, city: "New York" };
+// let object2 = { name: "Jane", age: 25, city: "San Francisco" };
+// let object3 = { name: "Jack", age: 40, city: "Los Angeles" };
+// let object4 = { name: "Jill", age: 35, city: "Chicago" };
+// let object5 = { name: "James", age: 45, city: "Miami" };
 
-let array = [object1, object2, object3, object4, object5];
+// let array = [object1, object2, object3, object4, object5];
 
-function test(array){
+// function test(array){
 
-for (let i=0; i<array.length;i++){     
-if (array[i].age>25)
-    {console.log(array[i]);
+// for (let i=0; i<array.length;i++){    
+//     if (array[i].name=="John" ) {
+// console.log(array[i]); break }
+// else {console.log ("not found")}}
+    
 
 
-}
 
-} 
 
-}test(array)
+// }
+
+// test(array)
+
+  
